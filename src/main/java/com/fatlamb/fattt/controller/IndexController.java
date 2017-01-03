ffcf140fe1ae308f1237588521873f59bc2c2a71
Mr-Song <xingxing1114@hotmail.com>
2017-01-03 20:42:24 +0800

    add template

---
+++ src/main/java/com/fatlamb/fattt/controller/IndexController.java
@@ -0,0 +1,7 @@
+package com.fatlamb.fattt.controller;
+
+/**
+ * Created by 58 on 2017/1/3.
+ */
+public class IndexController {
+}
---
+++ src/test/com.fatlamb.fattt.test/IndexControllerTest.java
@@ -0,0 +1,7 @@
+package com.fatlamb.fattt.test;
+
+/**
+ * Created by 58 on 2017/1/3.
+ */
+public class IndexControllerTest {
+}
